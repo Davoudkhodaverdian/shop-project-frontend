a0b6af6a0057e73009ca257b7b2ece2d751a6ba7
Davoudkhodaverdian <davoudkhodaverdian20@gmail.com>
2025-05-20 17:57:28 +0330

    update for build

--- src/fundamental/helpers/auth/index.ts
+++ src/fundamental/helpers/auth/index.ts
@@ -15,7 +15,8 @@ const storeLoginToken = async (token: string, days: number) => {
     const cookies = new Cookies();
     cookies.set('shop-token', token, {
         path: '/',
-        domain: process.env.NODE_ENV === 'development' ? 'localhost' : process.env.NEXT_APP_DOMAIN,
+        domain: 'localhost',
+        // domain: process.env.NODE_ENV === 'development' ? 'localhost' : process.env.NEXT_APP_DOMAIN,
         maxAge: 3600 * 24 * days
     });
 
